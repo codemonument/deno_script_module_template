c51fceddf7b0f292c9f7bc6b2ff2ca7eb55586d1
Benjamin Jesuiter <bjesuiter@gmail.com>
2022-04-20 22:08:12 +0200

    remove startKia from examples

--- example/main.ts
+++ example/main.ts
@@ -1,17 +1,9 @@
-import { startKia } from "../mod.ts";
+import { } from "../mod.ts";
 import { log } from "../dependencies/_log.std.ts";
 import { VERSION } from "../version.ts";
 
 try {
-  const demoKia = await startKia(
-    `Base Deno Module Example, Sleep for 3 sek`,
-  );
-  setTimeout(async () => {
-    await demoKia.succeed(
-      `Finished Base Deno Repo sucessfully}`,
-    );
-    log.info(`Module Version (version.ts): ${VERSION}`);
-  }, 3000);
+  log.info(`Module Version (version.ts): ${VERSION}`);
 } catch (error) {
   console.error(error);
   Deno.exit();
