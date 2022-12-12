3596b79e024672fb0248b4d6554dbc3954be896b
Benjamin Jesuiter <bjesuiter@gmail.com>
2022-12-12 01:40:31 +0100

    upgrade std deps

--- deps/std_testing.ts
+++ deps/std_testing.ts
@@ -1,3 +1,4 @@
-export * from "https://deno.land/std@0.162.0/testing/bdd.ts";
-export * from "https://deno.land/std@0.162.0/testing/asserts.ts";
-export * from "https://deno.land/std@0.162.0/testing/snapshot.ts";
+// eslint-disable-next-line deno-warn
+export * from "https://deno.land/std@0.167.0/testing/bdd.ts";
+export * from "https://deno.land/std@0.167.0/testing/asserts.ts";
+export * from "https://deno.land/std@0.167.0/testing/snapshot.ts";
