c952ad508382f9096a2d3eb73d07aebeebc3ece1
Benjamin Jesuiter <bjesuiter@gmail.com>
2023-03-19 16:58:31 +0100

    refactor example/main.ts

--- example/main.ts
+++ example/main.ts
@@ -1,9 +1,8 @@
-import {} from "@mod";
-import { VERSION } from "@version";
-import { log } from "@deps/std_log.ts";
+import { } from "@/mod.ts";
+import { log } from "@/deps/std.ts";
 
 try {
-  log.info(`Module Version (version.ts): ${VERSION}`);
+  log.info(`Your module is currently empty! Add some code to the lib folder and export it in mod.ts!`);
 } catch (error) {
   console.error(error);
   Deno.exit();
