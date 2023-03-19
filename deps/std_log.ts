32dfa4b1fad415ed31207d8c8a6e2b6a2579297c
Benjamin Jesuiter <bjesuiter@gmail.com>
2023-03-19 16:50:21 +0100

    improve std imports and upgrade versions

--- deps/std_log.ts
+++
@@ -1,1 +0,0 @@
-export * as log from "https://deno.land/std@0.167.0/log/mod.ts";
--- deps/std_testing.ts
+++ deps/std_testing.ts
@@ -1,4 +1,4 @@
 // eslint-disable-next-line deno-warn
-export * from "https://deno.land/std@0.167.0/testing/bdd.ts";
-export * from "https://deno.land/std@0.167.0/testing/asserts.ts";
-export * from "https://deno.land/std@0.167.0/testing/snapshot.ts";
+export * from "https://deno.land/std@0.180.0/testing/bdd.ts";
+export * from "https://deno.land/std@0.180.0/testing/asserts.ts";
+export * from "https://deno.land/std@0.180.0/testing/snapshot.ts";
